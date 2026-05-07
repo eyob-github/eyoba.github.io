11ce17092441454a5d19820eb0f4fc97f7d1d863
Eyob Berhe <59448510+eyob-github@users.noreply.github.com>
2026-05-07 18:19:28 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".highlight", {
+  strings: [
+    "Multi-Cloud Solutions Architect --AWS, Azure, GCP",
+    "Full Stack Developer",
+    "Platform Engineer",
+    "Software Engineer",
+    "DevSecOps",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
